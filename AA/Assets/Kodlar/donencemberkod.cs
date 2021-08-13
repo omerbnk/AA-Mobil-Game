22e4094075662b6d14afcc04b632ccb18b0200a8
omerbnk <73026541+omerbnk@users.noreply.github.com>
2021-08-13 15:56:49 +0300

    AA Mobil Game
    With Unity, I recoded the game named AA that existed in the story.

---
+++ AA/Assets/Kodlar/donencemberkod.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class donencemberkod : MonoBehaviour
+{
+
+    public float hız;
+    void Update()
+    {
+        transform.Rotate(0, 0, hız * Time.deltaTime);
+    }
+   
+}
---
+++ AA/Assets/Kodlar/oyunyoneticisi.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class oyunyoneticisi : MonoBehaviour
+{
+
+    GameObject donencember;
+    GameObject anacember;
+    public Animator animator;
+    void Start()
+    {
+        donencember = GameObject.FindGameObjectWithTag("donencember");
+        anacember = GameObject.FindGameObjectWithTag("anacember");
+    }
+
+    public void oyunBitti() {
+        anacember.GetComponent<anacemberkod>().enabled = false;
+        donencember.GetComponent<donencemberkod>().enabled = false;
+        animator.SetTrigger("oyunbitti");
+        StartCoroutine(loadScene());
+
+    }
+    IEnumerator loadScene() {
+        anacember.GetComponent<anacemberkod>().yanmak = false; 
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene("AnaMenu");
+      
+    }
+
+}
